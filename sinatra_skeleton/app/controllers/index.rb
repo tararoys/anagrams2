3e6a617f969c582f71300b963901f6875f70c62c
Tara and Danny <tlroys@gmail.com>
2014-02-17 17:40:32 -0600

    Added finding anagrams algorithm to post '/' in index.rb
    The post route can now locate anagrams for a particular word that it recieves as input.

--- sinatra_skeleton/app/controllers/index.rb
+++ sinatra_skeleton/app/controllers/index.rb
@@ -4,6 +4,22 @@ get '/' do
 end
 
 post '/' do
-  params[:word]
+  user_word = params[:word].chomp
+
+  @array = []
+  puts "doing post"
+  Word.all.each do |word|
+    puts word.word
+    puts user_word
+    if word.word.chars.sort == user_word.chars.sort
+      puts "yay a match"
+      @array << word.word
+    end
+  end
+
+  erb :word_list
+
+  # @array.inspect
+
 end
 
