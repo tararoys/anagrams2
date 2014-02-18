944c8ae5d874d8925e5ff0b1269c394e6812ce8a
batman <apprentice@dbc-chicago-63.local>
2014-02-17 18:46:51 -0600

    ripping out debugging statements

--- sinatra_skeleton/app/controllers/index.rb
+++ sinatra_skeleton/app/controllers/index.rb
@@ -7,19 +7,12 @@ post '/' do
   @user_word = params[:word].chomp
 
   @array = []
-  puts "doing post"
   Word.all.each do |word|
-    puts word.word
-    puts @user_word
     if word.word.chars.sort == @user_word.chars.sort
-      puts "yay a match"
       @array << word.word
     end
   end
 
   erb :word_list
-
-  # @array.inspect
-
 end
 
