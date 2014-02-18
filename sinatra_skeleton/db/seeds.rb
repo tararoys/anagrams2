4920f8f4e986e0d812bfa946381ad99f6f4092f5
batman <apprentice@dbc-chicago-63.local>
2014-02-17 19:01:02 -0600

    Second step to making anagrams fast to locate
    Add sorted word column to seeds.rb.  Created a chain of methods
    to sort each word before inserting it into the database.

--- sinatra_skeleton/db/seeds.rb
+++ sinatra_skeleton/db/seeds.rb
@@ -3,5 +3,5 @@
  text = File.open(APP_ROOT.join("db", 'test_dictionary.txt'))
 # text = File.open(APP_ROOT.join("db", 'test_dictionary.txt')).read
 text.each_line do |line|
-  Word.create(word: line.chomp)
+  Word.create(word: line.chomp, sorted_word: line.chomp.downcase.chars.sort.join)
 end
