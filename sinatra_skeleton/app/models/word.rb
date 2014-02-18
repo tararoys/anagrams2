64793d6956a219641c809c3f424c205db2598397
batman <apprentice@dbc-chicago-63.local>
2014-02-17 19:11:10 -0600

    Final step to making anagrams easy to access
    Don't search through the entire database when you have an index
    that lets you access the words you are looking for in log(n) time.
    
    Yay pretty one-line methods. :D

--- sinatra_skeleton/app/models/word.rb
+++ sinatra_skeleton/app/models/word.rb
@@ -1,9 +1,5 @@
 class Word < ActiveRecord::Base
   def self.anagram(user_word)
-    all.each_with_object([]) do |word, anagrams|
-      if word.sorted_word == user_word
-        anagrams << word.word
-      end
-    end
+    Word.where(sorted_word: user_word).map(&:word)
   end
 end
