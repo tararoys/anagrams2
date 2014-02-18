4c050ca6bc6ba87a2217c575a66ea36202dd9e19
batman <apprentice@dbc-chicago-63.local>
2014-02-17 18:51:34 -0600

    Extract find anagrams algorthim into Word class method
    To make the code more readable (and reusable), move anagram-finding
    logic from controller index.rb the Words model.  It fits better as
    a method than as a pile of logic hanging out in a post controller.

--- sinatra_skeleton/app/controllers/index.rb
+++ sinatra_skeleton/app/controllers/index.rb
@@ -6,12 +6,7 @@ end
 post '/' do
   @user_word = params[:word].chomp
 
-  @array = []
-  Word.all.each do |word|
-    if word.word.chars.sort == @user_word.chars.sort
-      @array << word.word
-    end
-  end
+  @array = Word.anagram(@user_word)
 
   erb :word_list
 end
--- sinatra_skeleton/app/models/word.rb
+++ sinatra_skeleton/app/models/word.rb
@@ -1,3 +1,10 @@
 class Word < ActiveRecord::Base
+  def self.anagram(user_word)
+    all.each_with_object([]) do |word, anagrams|
+      if word.word.chars.sort == user_word.chars.sort
+        anagrams << word.word
+      end
+    end
+  end
   # Remember to create a migration!
 end
